1c1658cfab49e472b8bd219c1e805a30583f4263
tmbb-japan <woo787@naver.com>
2023-07-08 23:17:26 +0900

    feat: factory on model side

--- lib/restaurant/model/restaurant_model.dart
+++ lib/restaurant/model/restaurant_model.dart
@@ -1,3 +1,6 @@
+import 'package:delivery/common/const/data.dart';
+import 'package:flutter/material.dart';
+
 enum RestaurantPriceRange {
   expensive,
   medium,
@@ -26,4 +29,22 @@ class RestaurantModel {
     required this.deliveryTime,
     required this.deliveryFee,
   });
+
+  factory RestaurantModel.fromJson({
+    required Map<String, dynamic> json,
+  }) {
+    return RestaurantModel(
+      id: json['id'],
+      name: json['name'],
+      thumbUrl: 'http://$ip${json['thumbUrl']}',
+      tags: List<String>.from(json['tags']),
+      priceRange: RestaurantPriceRange.values.firstWhere(
+        (e) => e.name == json['priceRange'],
+      ),
+      ratings: json['ratings'],
+      ratingsCount: json['ratingsCount'],
+      deliveryTime: json['deliveryTime'],
+      deliveryFee: json['deliveryFee'],
+    );
+  }
 }
