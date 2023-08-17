77026b158888d817ac5cfa05fdef67c15c70e590
tmbb-japan <woo787@naver.com>
2023-08-17 14:16:40 +0900

    feat : feat : cache the detail screen through provider_2

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -11,7 +11,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantDetailScreen extends ConsumerWidget {
+class RestaurantDetailScreen extends ConsumerStatefulWidget {
   final String id;
 
   const RestaurantDetailScreen({
@@ -20,8 +20,22 @@ class RestaurantDetailScreen extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(restaurantDetailProvider(id));
+  ConsumerState<RestaurantDetailScreen> createState() =>
+      _RestaurantDetailScreenState();
+}
+
+class _RestaurantDetailScreenState
+    extends ConsumerState<RestaurantDetailScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(restaurantProvider.notifier).getDetail(id: widget.id);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(restaurantDetailProvider(widget.id));
 
     if (state == null) {
       return DefaultLayout(
@@ -38,60 +52,61 @@ class RestaurantDetailScreen extends ConsumerWidget {
           renderTop(
             model: state,
           ),
-          // renderLabel(),
-          // renderProducts(
-          //   products: snapshot.data!.products,
-          // ),
+          if (state is RestaurantDetailModel) renderLabel(),
+          if (state is RestaurantDetailModel)
+            renderProducts(
+              products: state.products,
+            ),
         ],
       ),
     );
   }
-}
 
-SliverPadding renderLabel() {
-  return SliverPadding(
-    padding: EdgeInsets.symmetric(horizontal: 16.0),
-    sliver: SliverToBoxAdapter(
-      child: Text(
-        '메뉴',
-        style: TextStyle(
-          fontSize: 18.0,
-          fontWeight: FontWeight.w500,
+  SliverPadding renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-SliverPadding renderProducts({
-  required List<RestaurantProductModel> products,
-}) {
-  return SliverPadding(
-    padding: EdgeInsets.symmetric(horizontal: 16.0),
-    sliver: SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (context, index) {
-          final model = products[index];
-          return Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: ProductCard.fromModel(
-              model: model,
-            ),
-          );
-        },
-        childCount: products.length,
+  SliverPadding renderProducts({
+    required List<RestaurantProductModel> products,
+  }) {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            final model = products[index];
+            return Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ProductCard.fromModel(
+                model: model,
+              ),
+            );
+          },
+          childCount: products.length,
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-SliverToBoxAdapter renderTop({
-  required RestaurantModel model,
-}) {
-  return SliverToBoxAdapter(
-    child: RestaurantCard.fromModel(
-      model: model,
-      isDetail: true,
-    ),
-  );
+  SliverToBoxAdapter renderTop({
+    required RestaurantModel model,
+  }) {
+    return SliverToBoxAdapter(
+      child: RestaurantCard.fromModel(
+        model: model,
+        isDetail: true,
+      ),
+    );
+  }
 }
