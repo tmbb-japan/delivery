7697cd0cf927fcea3f4b04a50ddd110039036245
tmbb-japan <woo787@naver.com>
2023-08-17 14:24:13 +0900

    feat : heroWidget on detailScreen image

--- lib/restaurant/component/restaurant_card.dart
+++ lib/restaurant/component/restaurant_card.dart
@@ -20,6 +20,8 @@ class RestaurantCard extends StatelessWidget {
   final double ratings;
   // 상세 카드 여부
   final bool isDetail;
+  //Hero 위젯 태그
+  final String? heroKey;
   // 상세 내용
   final String? detail;
 
@@ -34,6 +36,7 @@ class RestaurantCard extends StatelessWidget {
     required this.ratings,
     this.isDetail = false,
     this.detail,
+    this.heroKey,
   });
 
   factory RestaurantCard.fromModel({
@@ -45,6 +48,7 @@ class RestaurantCard extends StatelessWidget {
         model.thumbUrl,
         fit: BoxFit.cover,
       ),
+      heroKey: model.id,
       name: model.name,
       tags: model.tags,
       ratingsCount: model.ratingsCount,
@@ -60,10 +64,17 @@ class RestaurantCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        if (isDetail) image,
-        if (!isDetail)
+        if (heroKey != null)
+          Hero(
+            tag: ObjectKey(heroKey),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(isDetail ? 0 : 12.0),
+              child: image,
+            ),
+          ),
+        if (heroKey == null)
           ClipRRect(
-            borderRadius: BorderRadius.circular(12.0),
+            borderRadius: BorderRadius.circular(isDetail ? 0 : 12.0),
             child: image,
           ),
         const SizedBox(height: 16.0),
