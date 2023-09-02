556b6c53cccb9192551fac912ebca78a0d482c3f
tmbb-japan <woo787@naver.com>
2023-09-02 18:42:07 +0900

    feat : RestaurantRatingRepository

---
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -0,0 +1,22 @@
+import 'package:delivery/common/model/cursor_pagination_model.dart';
+import 'package:delivery/common/model/pagination_params.dart';
+import 'package:delivery/rating/model/rating_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/retrofit.dart';
+
+part 'restaurant_rating_repository.g.dart';
+
+// http://ip/restaurant/:rid/rating
+@RestApi()
+abstract class RestaurantRatingRepository {
+  factory RestaurantRatingRepository(Dio dio, {String baseUrl}) =
+      _RestaurantRatingRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPagination<RatingModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+}
