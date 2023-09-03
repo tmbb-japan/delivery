ad523c9c144dd6a21fa18966ff35acf09ed51e6e
tmbb-japan <woo787@naver.com>
2023-09-03 23:30:27 +0900

    feat : make Ratingrepository be provider

--- lib/restaurant/repository/restaurant_rating_repository.dart
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -1,10 +1,19 @@
+import 'package:delivery/common/dio/dio.dart';
 import 'package:delivery/common/model/cursor_pagination_model.dart';
 import 'package:delivery/common/model/pagination_params.dart';
 import 'package:delivery/rating/model/rating_model.dart';
 import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'restaurant_rating_repository.g.dart';
+
+final RestaurantRatingRepositoryProvider =
+    Provider.family<RestaurantRatingRepository, String>((ref, id) {
+  final dio = ref.watch(dioProvider);
+  return RestaurantRatingRepository(dio,
+      baseUrl: 'http://$ip/restaurant/$id/rating');
+});
 
 // http://ip/restaurant/:rid/rating
 @RestApi()
