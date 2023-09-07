d32184a7c332b543c932e14861baaf9d384c7676
tmbb-japan <woo787@naver.com>
2023-09-07 16:53:58 +0900

    feat : Rating Pagination

--- lib/restaurant/provider/restaurant_rating_provider.dart
+++ lib/restaurant/provider/restaurant_rating_provider.dart
@@ -1,13 +1,18 @@
 import 'package:delivery/common/model/cursor_pagination_model.dart';
+import 'package:delivery/common/provider/pagination_provider.dart';
+import 'package:delivery/rating/model/rating_model.dart';
 import 'package:delivery/restaurant/repository/restaurant_rating_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final restaurantRatingProvider = StateNotifierProvider.family<
+    RestaurantRatingStateNotifier, CursorPaginationBase, String>((ref, id) {
+  final repo = ref.watch(RestaurantRatingRepositoryProvider(id));
+  return RestaurantRatingStateNotifier(repository: repo);
+});
+
 class RestaurantRatingStateNotifier
-    extends StateNotifier<CursorPaginationBase> {
-  final RestaurantRatingRepository repository;
+    extends PaginationProvider<RatingModel, RestaurantRatingRepository> {
   RestaurantRatingStateNotifier({
-    required this.repository,
-  }) : super(
-          CursorPaginationLoading(),
-        );
+    required super.repository,
+  });
 }
--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -7,6 +7,7 @@ import 'package:delivery/restaurant/component/restaurant_card.dart';
 import 'package:delivery/restaurant/model/restaurant_detail_model.dart';
 import 'package:delivery/restaurant/model/restaurant_model.dart';
 import 'package:delivery/restaurant/provider/restaurant_provider.dart';
+import 'package:delivery/restaurant/provider/restaurant_rating_provider.dart';
 import 'package:delivery/restaurant/repository/restaurant_repository.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -38,7 +39,9 @@ class _RestaurantDetailScreenState
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(restaurantDetailProvider(widget.id));
+    final ratingsState = ref.watch(restaurantRatingProvider(widget.id));
 
+    print(ratingsState);
     if (state == null) {
       return DefaultLayout(
         child: Center(
