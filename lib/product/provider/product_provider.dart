b4bcdb4490177496ed8ffff954772efeb3ebd656
tmbb-japan <woo787@naver.com>
2023-09-12 18:05:11 +0900

    feat : product pagination setting

---
+++ lib/product/provider/product_provider.dart
@@ -0,0 +1,18 @@
+import 'package:delivery/common/model/cursor_pagination_model.dart';
+import 'package:delivery/common/provider/pagination_provider.dart';
+import 'package:delivery/product/model/product_model.dart';
+import 'package:delivery/product/repository/product_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final productProvider =
+    StateNotifierProvider<ProductStateNotifier, CursorPaginationBase>((ref) {
+  final repo = ref.watch(productRepositoryProvider);
+  return ProductStateNotifier(repository: repo);
+});
+
+class ProductStateNotifier
+    extends PaginationProvider<ProductModel, ProductRepository> {
+  ProductStateNotifier({
+    required super.repository,
+  });
+}
---
+++ lib/product/repository/product_repository.dart
@@ -0,0 +1,30 @@
+import 'package:delivery/common/dio/dio.dart';
+import 'package:delivery/common/model/cursor_pagination_model.dart';
+import 'package:delivery/common/model/pagination_params.dart';
+import 'package:delivery/common/repository/base_pagination_repository.dart';
+import 'package:delivery/product/model/product_model.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'product_repository.g.dart';
+
+final productRepositoryProvider = Provider<ProductRepository>(
+  (ref) {
+    final dio = ref.watch(dioProvider);
+    return ProductRepository(dio, baseUrl: 'http://$ip/product');
+  },
+);
+
+// http://$ip/product
+@RestApi()
+abstract class ProductRepository
+    implements IBasePaginationRepository<ProductModel> {
+  factory ProductRepository(Dio dio, {String baseUrl}) = _ProductRepository;
+
+  @GET('/')
+  @Headers({'accessToken': 'true'})
+  Future<CursorPagination<ProductModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+}
