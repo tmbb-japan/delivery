572f000493713bc482dbdf176b1ca9c60263c10e
tmbb-japan <woo787@naver.com>
2023-10-14 22:56:30 +0900

    feat : productTab

--- lib/product/view/product_screen.dart
+++ lib/product/view/product_screen.dart
@@ -1,20 +1,19 @@
+import 'package:delivery/common/component/pagination_list_view.dart';
+import 'package:delivery/product/component/product_card.dart';
+import 'package:delivery/product/model/product_model.dart';
 import 'package:delivery/product/provider/product_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductScreen extends ConsumerStatefulWidget {
-  const ProductScreen({super.key});
-
-  @override
-  ConsumerState<ProductScreen> createState() => _ProductTabState();
-}
-
-class _ProductTabState extends ConsumerState<ProductScreen> {
+class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final state = ref.watch(productProvider);
-    return Center(
-      child: Text('음식'),
+    return PaginationListView<ProductModel>(
+      provider: productProvider,
+      itemBuilder: <ProductModel>(_, index, model) {
+        return ProductCard.fromProductModel(
+          model: model,
+        );
+      },
     );
   }
 }
